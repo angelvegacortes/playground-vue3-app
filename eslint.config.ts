47ce023f7cdf434a20bef0a8dd9ff10c3094f04a
Angel Vega <angelvegacortes@gmail.com>
2025-04-09 22:14:08 -0400

    Added more eslint plugins

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,49 +1,38 @@
 import pluginVue from 'eslint-plugin-vue'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import boundaries from "eslint-plugin-boundaries";
-import typescriptParser from "@typescript-eslint/parser";
-import typescriptEslintPlugin from "@typescript-eslint/eslint-plugin";
+import importPlugin from "eslint-plugin-import";
+// import typescriptParser from "@typescript-eslint/parser";
+// import typescriptEslintPlugin from "@typescript-eslint/eslint-plugin";
 import pluginVitest from '@vitest/eslint-plugin'
 import pluginPlaywright from 'eslint-plugin-playwright'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
-
-// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
-// import { configureVueProject } from '@vue/eslint-config-typescript'
-// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
-// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
+import cspellPlugin from '@cspell/eslint-plugin';
 
 export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
+    files: ['**/*.{ts,vue}'],
   },
   {
     name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    ignores: [
+      '**/dist/**',
+      '**/coverage/**'
+    ],
   },
+  pluginVue.configs['flat/essential'],
+  vueTsConfigs.recommended,
   {
-    languageOptions: {
-      parser: typescriptParser,
-    },
     plugins: {
-      "@typescript-eslint": typescriptEslintPlugin,
-      boundaries,
-    },
-    rules: {
-      ...boundaries.configs.strict.rules,
-      "boundaries/external": ["off"]
+      boundaries
     },
     settings: {
-      "import/resolver": {
-        typescript: {
-          alwaysTryTypes: true,
-        },
-      },
       "boundaries/include": ["src/**/*"],
-      "boundaries/ignore": ["**/*.spec.ts"],
       "boundaries/elements": [
         {
-          type: "shared",
+          mode: "full",
+          type: "global-level-resource",
           pattern: [
             "src/assets/**",
             "src/components/**",
@@ -52,28 +41,66 @@ export default defineConfigWithVueTs(
           ]
         },
         {
-          type: "never-import",
+          mode: "full",
+          type: "app-level-resource",
           pattern: [
             "src/views/**",
-            "src/App.vue",
-            "src/main.ts"
+            "src/*",
           ]
         },
         {
-          type: "feature",
+          mode: "full",
+          type: "feature-level-resource",
           capture: ["featureName"],
-          pattern: "src/features/*/**"
+          pattern: "src/features/*/**/*"
         }
       ]
     },
+    rules: {
+      "boundaries/no-unknown": ["error"],
+      "boundaries/no-unknown-files": ["error"],
+      "boundaries/element-types": [
+        "error", {
+          "default": "disallow",
+          "rules": [
+            {
+              "from": ["global-level-resource"],
+              "allow": ["app-level-resource", "global-level-resource"],
+            },
+            {
+              "from": ["app-level-resource"],
+              "allow": ["app-level-resource", "global-level-resource"],
+            },
+            {
+              "from": ["feature-level-resource"],
+              "allow": [
+                "global-level-resource",
+                ["feature-level-resource", { "featureName": "${from.featureName}"}]
+              ],
+            }
+          ] 
+        }
+      ]
+    }
   },
-  pluginVue.configs['flat/essential'],
-  vueTsConfigs.recommended,
+  {
+    extends: [importPlugin.flatConfigs.recommended, importPlugin.flatConfigs.typescript],
+    rules: {
+      'no-unused-vars': 'off',
+      'import/no-unresolved': 'off'
+    }
+  },
+  {
+    plugins: { '@cspell': cspellPlugin },
+    files: ['src/**/*.{ts,vue}'],
+    rules: {
+      '@cspell/spellchecker': ['warn', {}]
+    }
+  },
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
-  
   {
     ...pluginPlaywright.configs['flat/recommended'],
     files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
--- src/features/featureB/BTestParent.vue
+++ src/features/featureB/BTestParent.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import ATestChild from '../featureA/ATestChild.vue'
-import CTestChild from '../featureC/CTestChild.vue'
+// import ATestChild from '../featureA/ATestChild.vue'
+// import CTestChild from '../featureC/CTestChild.vue'
 
 </script>
 
 <template>
   <div>
     <span>Feature B parent component.</span>
-    <ATestChild />
-    <CTestChild />
+    <!-- <ATestChild /> -->
+    <!-- <CTestChild /> -->
   </div>
 </template>
