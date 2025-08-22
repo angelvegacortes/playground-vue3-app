5c0fc101647e22f411dc0946f021c7e1cc59c1a6
Angel Vega <angelvegacortes@gmail.com>
2025-08-22 14:34:39 -0400

    Added support for nested env vars

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="footer-bg-color text-center text-black">{{ title }} | Version: {{ version }}</div>
+  <div class="footer-bg-color text-center text-black">
+    {{ title }} | Version: {{ version }} | {{ date }}
+  </div>
 </template>
 
 <script setup lang="ts">
 const title = import.meta.env.VITE_APP_TITLE
 const version = import.meta.env.VITE_APP_VERSION
+const date = import.meta.env.VITE_APP_DATE
 </script>
 
 <style scoped>
