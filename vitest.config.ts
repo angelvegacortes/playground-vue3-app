3ed673b31a6c18d8a74d0350b44a25358d0b745c
Angel Vega <angelvegacortes@gmail.com>
2025-09-10 22:35:14 -0400

    Enable vitest code coverage thresholds

--- vitest.config.ts
+++ vitest.config.ts
@@ -15,14 +15,20 @@ export default defineConfig((configEnv) =>
         coverage: {
           provider: 'v8',
           reporter: ['text', 'json', 'html'],
+          thresholds: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80
+          },
           include: ['src/**/*.{ts,vue}'],
           exclude: [
             'public/**',
-            'src/App.vue',
+            'src/app.vue',
             'src/main.ts',
             'src/**/types/**',
             'src/config/**',
-            'src/**/mocks/**',
+            'src/mocks/**',
           ],
         },
       },
