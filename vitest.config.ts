606e009d4f915bae3628d4b22485824b1a2d9939
Angel Vega <angelvegacortes@gmail.com>
2025-09-26 11:05:34 -0400

    Updated outdated npm packages

--- vitest.config.ts
+++ vitest.config.ts
@@ -19,7 +19,7 @@ export default defineConfig((configEnv) =>
             branches: 80,
             functions: 80,
             lines: 80,
-            statements: 80
+            statements: 80,
           },
           include: ['src/**/*.{ts,vue}'],
           exclude: [
