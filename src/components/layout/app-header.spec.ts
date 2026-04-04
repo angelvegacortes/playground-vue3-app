461d2fa269ac843e7fd8b582d462973fff89020f
Angel Vega <angelvegacortes@gmail.com>
2026-04-04 12:01:30 -0400

    Update menubar. Minor code refactoring.

--- src/components/layout/app-header.spec.ts
+++ src/components/layout/app-header.spec.ts
@@ -1,6 +1,7 @@
 import router from '@app/router'
 import { server } from '@app/services/mocks/node'
 import { createTestingPinia } from '@pinia/testing'
+import userEvent from '@testing-library/user-event'
 import { render, screen } from '@testing-library/vue'
 import { afterAll, afterEach, beforeAll, describe, expect, it } from 'vitest'
 import AppHeader from './app-header.vue'
@@ -22,7 +23,7 @@ describe('app-header', () => {
     expect(screen.getByText('Vue 3 Playground')).toBeTruthy()
   })
 
-  it('shows menu links', () => {
+  it('shows menu items', () => {
     render(AppHeader, {
       global: {
         plugins: [router, createTestingPinia({ stubActions: false })],
@@ -31,11 +32,42 @@ describe('app-header', () => {
 
     expect(screen.getByRole('link', { name: 'Home' })).toBeTruthy()
     expect(screen.getByRole('link', { name: 'Permissions' })).toBeTruthy()
+    expect(screen.getByRole('button', { name: 'Logout' })).toBeTruthy()
+  })
+
+  it('shows layout menu item and its nested links', async () => {
+    render(AppHeader, {
+      global: {
+        plugins: [router, createTestingPinia({ stubActions: false })],
+      },
+    })
+
+    const button = screen.getByRole('button', { name: 'Layout' })
+
+    expect(button).toBeTruthy()
+
+    await userEvent.click(button)
+
     expect(screen.getByRole('link', { name: 'Tabs' })).toBeTruthy()
     expect(screen.getByRole('link', { name: 'Data Tables' })).toBeTruthy()
     expect(screen.getByRole('link', { name: 'Calendars' })).toBeTruthy()
+  })
+
+  it('shows inputs menu item and its nested links', async () => {
+    render(AppHeader, {
+      global: {
+        plugins: [router, createTestingPinia({ stubActions: false })],
+      },
+    })
+
+    const button = screen.getByRole('button', { name: 'Inputs' })
+
+    expect(button).toBeTruthy()
+
+    await userEvent.click(button)
+
     expect(screen.getByRole('link', { name: 'Forms' })).toBeTruthy()
-    expect(screen.getByRole('link', { name: 'Inputs' })).toBeTruthy()
-    expect(screen.getByRole('link', { name: 'Logout' })).toBeTruthy()
+    expect(screen.getByRole('link', { name: 'Input Fields' })).toBeTruthy()
+    expect(screen.getByRole('link', { name: 'Keyboard Shortcuts' })).toBeTruthy()
   })
 })
--- src/components/layout/app-header.vue
+++ src/components/layout/app-header.vue
@@ -1,99 +1,40 @@
 <template>
-  <!-- <Menubar v-if="userStore.isAuthenticated() && userStore.hasAcceptedTerms()"> -->
-  <Menubar>
+  <Menubar :model="items">
     <template #start>
       <div class="mt-2 flex items-center gap-2">
         <span>{{ title }}</span>
-        <Button v-tooltip.bottom="'Home'" size="small" as="RouterLink" to="/" label="Home">
-          <span class="material-symbols-rounded">home</span>
-          <span>Home</span>
-        </Button>
-        <Button
-          v-tooltip.bottom="'Permission Examples'"
-          size="small"
-          as="RouterLink"
-          to="/examples/permissions"
-          label="Permissions"
-        >
-          <span class="material-symbols-rounded">security</span>
-          <span>Permissions</span>
-        </Button>
-        <Button
-          v-tooltip.bottom="'Tab Examples'"
-          size="small"
-          as="RouterLink"
-          to="/examples/tabs"
-          label="Tabs"
-        >
-          <span class="material-symbols-rounded">tabs</span>
-          <span>Tabs</span>
-        </Button>
-        <Button
-          v-tooltip.bottom="'Data Table Examples'"
-          size="small"
-          as="RouterLink"
-          to="/examples/data-tables"
-          label="Data Tables"
-        >
-          <span class="material-symbols-rounded">data_table</span>
-          <span>Data Tables</span>
-        </Button>
-        <Button
-          v-tooltip.bottom="'Calendar Examples'"
-          as="RouterLink"
-          to="/examples/calendars"
-          size="small"
-          label="Calendars"
-        >
-          <span class="material-symbols-rounded">calendar_month</span>
-          <span>Calendars</span>
-        </Button>
-        <Button
-          v-tooltip.bottom="'Form Examples'"
-          as="RouterLink"
-          to="/examples/forms"
-          size="small"
-          label="Forms"
-        >
-          <span class="material-symbols-rounded">forms_add_on</span>
-          <span>Forms</span>
-        </Button>
-        <Button
-          v-tooltip.bottom="'Input Examples'"
-          as="RouterLink"
-          to="/examples/inputs"
-          size="small"
-          label="Inputs"
-        >
-          <span class="material-symbols-rounded">input</span>
-          <span>Inputs</span>
-        </Button>
-        <Button
-          v-tooltip.bottom="'PDF Examples'"
-          as="RouterLink"
-          to="/examples/pdfs"
-          size="small"
-          label="PDFs"
-        >
-          <span class="material-symbols-rounded">picture_as_pdf</span>
-          <span>PDFs</span>
-        </Button>
-        <slot name="notificationsButton"></slot>
       </div>
     </template>
+    <template #item="{ item, root }">
+      <Button
+        v-if="item.route"
+        size="small"
+        as="RouterLink"
+        :variant="!root ? 'text' : undefined"
+        :severity="!root ? 'success' : undefined"
+        :to="item.route"
+        :label="item.label as string"
+      >
+        <span class="material-symbols-rounded">{{ item.icon }}</span>
+        <span>{{ item.label }}</span>
+      </Button>
+      <Button v-else size="small" :label="item.label as string">
+        <span>{{ item.label }}</span>
+        <span class="material-symbols-rounded">keyboard_arrow_down</span>
+      </Button>
+    </template>
     <template #end>
-      <div class="mt-2 flex items-center gap-2">
+      <div class="mt-2 flex items-center gap-4">
         <span>Logged in as {{ userStore.getName() }}</span>
+        <slot name="notificationsButton"></slot>
         <Button
-          as="RouterLink"
-          to="/login"
+          v-tooltip.left="'Logout'"
           size="small"
           severity="secondary"
           label="Logout"
-          @click="userStore.logout"
+          @click="logout()"
         >
           <span class="material-symbols-rounded">logout</span>
-          <span>Logout</span>
         </Button>
       </div>
     </template>
@@ -101,7 +42,9 @@
 </template>
 
 <script setup lang="ts">
+import router from '@app/router'
 import { useUserStore } from '@app/stores/user-store'
+import { ref } from 'vue'
 
 defineSlots<{
   notificationsButton(): void
@@ -109,4 +52,69 @@ defineSlots<{
 
 const userStore = useUserStore()
 const title = import.meta.env.VITE_APP_TITLE
+
+const logout = () => {
+  userStore.logout()
+  router.push('/login')
+}
+
+const items = ref([
+  {
+    label: 'Home',
+    icon: 'home',
+    route: '/',
+  },
+  {
+    label: 'Permissions',
+    icon: 'security',
+    route: '/examples/permissions',
+  },
+  {
+    label: 'Layout',
+    icon: 'security',
+    items: [
+      {
+        label: 'Calendars',
+        icon: 'calendar_month',
+        route: '/examples/calendars',
+      },
+      {
+        label: 'Tabs',
+        icon: 'tabs',
+        route: '/examples/tabs',
+      },
+      {
+        label: 'Data Tables',
+        icon: 'data_table',
+        route: '/examples/data-tables',
+      },
+    ],
+  },
+  {
+    label: 'Inputs',
+    icon: 'security',
+    items: [
+      {
+        label: 'Input Fields',
+        icon: 'input',
+        route: '/examples/inputs',
+      },
+      {
+        label: 'Forms',
+        icon: 'forms_add_on',
+        route: '/examples/forms',
+      },
+      {
+        label: 'Keyboard Shortcuts',
+        icon: 'keyboard',
+        route: '/examples/keyboard-shortcuts',
+      },
+    ],
+  },
+  {
+    label: 'PDFs',
+    icon: 'picture_as_pdf',
+    route: '/examples/pdfs',
+  },
+])
 </script>
